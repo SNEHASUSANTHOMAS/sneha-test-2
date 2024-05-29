f22af3c9af85b55209a478ebed1fe54a3142ef36
susan <sneha1995susan@gmail.com>
2024-05-29 16:30:48 +0530

    add contact category about pages

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
             this.menuIcon=$(".menu-icon");
             this.topNavItems=$(".top-nav-items");
             this.closeIcon=$(".close-icon");
-            console.log(this.navItems);
+            
         },
         eventHandler:function(){
-            console.log("hi");
+            
             let that=this;
             this.menuIcon.click(function(){
                 that.topNavItems.toggleClass('show');
             });
             this.closeIcon.click(function(){
-                console.log("close");
+              
                 that.topNavItems.removeClass('show');
 
             })
